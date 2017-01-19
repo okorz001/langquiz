3eb7a3d3e4dc2c2db97f11f2956468fee2711991
Oscar Korz <okorz001@gmail.com>
2017-01-18 19:56:21 -0800

    sortable dictionary

--- client/components/Dictionary.js
+++ client/components/Dictionary.js
@@ -1,60 +1,21 @@
 import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
 
-const perc = (correct, total) =>
-    total == 0 ? 0 : Math.floor(100 * correct / total)
-
-const Definition = ({native, foreign, correct, total}) => (
-    <tr className="definition">
-        <td className="native">
-            {native}
-        </td>
-        <td className="foreign">
-            {foreign}
-        </td>
-        <td className="history total">
-            {`${correct}/${total}`}
-        </td>
-        <td className="history percent">
-            {`(${perc(correct, total)}%)`}
-        </td>
-    </tr>
-)
-
-Definition.propTypes = {
-    native: PropTypes.string.isRequired,
-    foreign: PropTypes.string.isRequired,
-    correct: PropTypes.number.isRequired,
-    total: PropTypes.number.isRequired,
-}
+import SortableTable, {compareNumbers} from './SortableTable'
 
 const Dictionary = ({active, words, history}) => {
-    const defs = words
-        // TODO: allow other sorting
-        .sort((a, b) => a.native.localeCompare(b.native))
-        .map(word => {
-            const {id, native, foreign} = word
-            const historyForWord = history[id] || {}
-            const correct = historyForWord.correct || 0
-            const total = historyForWord.total || 0
-            return <Definition key={id} native={native} foreign={foreign}
-                               correct={correct} total={total} />
-        })
-    // TODO: better table headers
-    return (
-        <table className="dictionary">
-            <thead>
-                <tr>
-                    <th>{active.native}</th>
-                    <th>{active.foreign}</th>
-                    <th colSpan="2">History</th>
-                </tr>
-            </thead>
-            <tbody>
-                {defs}
-            </tbody>
-        </table>
-    )
+    const columns = [
+        {label: active.native, sort: 'string'},
+        {label: active.foreign, sort: 'string'},
+        {label: 'history'},
+    ]
+    const data = words.map(word => {
+        const {id, native, foreign} = word
+        const {correct, total} = history[id] || {correct: 0, total: 0}
+        return [native, foreign, `${correct}/${total}`]
+    })
+    return <SortableTable className="dictionary"
+                          columns={columns} data={data} sort={0} />
 }
 
 Dictionary.propTypes = {
