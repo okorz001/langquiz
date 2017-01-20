b9601a8a5554811ae43644d88c2a3bd708695844
Oscar Korz <okorz001@gmail.com>
2017-01-20 10:37:01 -0800

    use correct spelling for correct answer

--- client/reducers/recent.js
+++ client/reducers/recent.js
@@ -9,6 +9,11 @@ export default (state = [], action) => {
                 answer: action.quiz.answer,
                 correct: action.correct,
             }
+
+            // If the user is right, just use expected since answer may have
+            // strange capitalization. It's easier to fudge here than with CSS
+            if (result.correct) result.answer = result.expected
+
             // Prepend new result to front of array
             return [result].concat(state)
     }
