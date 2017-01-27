fa2c02340d94be7b3e4df9129bf5abbec1098b80
Oscar Korz <okorz001@gmail.com>
2017-01-26 16:34:36 -0800

    override api host from query string

--- client/actions/loadWords.js
+++ client/actions/loadWords.js
@@ -1,7 +1,12 @@
 import fetch from 'isomorphic-fetch'
+import {parse} from 'query-string'
 
 import * as actionTypes from '../actionTypes'
 import {nextQuestion} from './quiz'
+
+// should put this in app state if it changes
+const QUERY = parse(window.location.search)
+const API_HOST = QUERY.apiHost ? '//' + QUERY.apiHost : ''
 
 const loadWordsBegin = (key) => ({
     type: actionTypes.LOAD_WORDS,
@@ -22,7 +27,7 @@ const loadWordsFail = (key, err) => ({
 
 export default (key) => (dispatch) => {
     dispatch(loadWordsBegin(key))
-    return fetch(`/api/words/${key}`)
+    return fetch(`${API_HOST}/api/words/${key}`)
         .then(response => response.json())
         .then(json => dispatch(loadWordsDone(key, json.words)))
         .then(() => dispatch(nextQuestion()))
